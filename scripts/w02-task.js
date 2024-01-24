17958b972272e938d28979bddd3baca69465e064
ferdinandstanley <145779786+ferdinandstanley@users.noreply.github.com>
2024-01-24 23:26:35 +0100

    changes updated

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,26 +3,35 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
+const fullName = "Ferdinand Stanley Uchechukwu";
+const currentYear = new Date ().getFullYear(); // 2024
+const profilePicture = "images/ferdinand_stanley_uchechukwu.jpg";
 
 
 /* Step 3 - Element Variables */
 
-
-
+const foodElement = document.querySelector('#food');
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector('img');
 
 
 /* Step 4 - Adding Content */
 
-
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `This is ${fullName}. He is a web developer`);
 
 
 
 
 /* Step 5 - Array */
 
+let favfoods = ["pizza", "rice", "fish pepper soup", "chicken pie"];
 
+foodElement.innerHTML = favfoods
+let newfood = 'Garri and egusi soup'
+favfoods.push(newfood);
+foodElement.innerHTML += `<br>${favfoods}`;
 
 
 
